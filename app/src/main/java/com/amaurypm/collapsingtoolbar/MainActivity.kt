665a3dea48a01cd0852c563c7ee2571196f56b39
AmauryPM <amaury.matsumura@gmail.com>
2023-01-06 18:50:30 -0600

    Commit inicial

---
+++ app/src/main/java/com/amaurypm/collapsingtoolbar/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.amaurypm.collapsingtoolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import com.amaurypm.collapsingtoolbar.databinding.ActivityMainBinding
+import com.amaurypm.collapsingtoolbar.databinding.PruebadiplomovilesBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        setSupportActionBar(binding.toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_search -> {
+                Toast.makeText(this, "Click en botón search", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_share -> {
+                Toast.makeText(this, "Click en botón share", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
