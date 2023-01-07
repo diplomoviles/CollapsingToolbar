7aaee221cc2513ae4ff07bcec12d10192c185d52
AmauryPM <amaury.matsumura@gmail.com>
2023-01-06 19:25:07 -0600

    Con include en el layout del main activity

--- app/src/main/java/com/amaurypm/collapsingtoolbar/MainActivity.kt
+++ app/src/main/java/com/amaurypm/collapsingtoolbar/MainActivity.kt
@@ -7,21 +7,24 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.amaurypm.collapsingtoolbar.databinding.ActivityMainBinding
-import com.amaurypm.collapsingtoolbar.databinding.PruebadiplomovilesBinding
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         //val toolbar = findViewById<Toolbar>(R.id.toolbar)
 
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(binding.ctLayout.toolbar)
 
     }
 
